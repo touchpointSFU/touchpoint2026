0279c88ba8c60284f68f5b971b05324fec178878
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2026-01-17 01:05:34 -0800

    fix: reset favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           name="description"
           content="Your friendly neighbourhood design conference"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Touchpoint 2026" />
