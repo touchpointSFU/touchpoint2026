b50248d17f80f5a9ea8c56c9ceaf164c460d0fa7
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2026-01-16 23:38:06 -0800

    fix: merge layers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import circleInnovation from "/public/circle-innovation-logo-white.png";
 
+import { Fragment, useEffect, useMemo, useRef } from "react";
+import { Mesh, Program, RenderTarget, Triangle, Vec3 } from "ogl";
+
+import { OGLCanvas } from "@/components/OGLCanvas/OGLCanvas";
+import { FinalScene } from "@/components/Shaders/FinalScene";
+import { useFrame, useOGL } from "react-ogl";
+
+import basicVert from "@/components/Shaders/basic.vert";
+import basicFrag from "@/components/Shaders/metaballs.frag";
+
 export default function Home() {
   return (
     <motion.div
@@ -13,25 +23,134 @@ export default function Home() {
       key="home-page"
       className={`flex flex-col leading-none min-h-dvh items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
-      <h1 className="font-display text-[#FF39E1] mb-4 md:mb-8 min-[1920px]:mb-10.5 text-display font-semibold">
-        Touchpoint 2026
-      </h1>
-      <p className="text-description font-Haas text-touchpoint-green mb-2 md:mb-4 min-[1920px]:mb-6 ">
-        SFU Surrey Engineering Building
-      </p>
-      <time
-        className="text-touchpoint-green text-description font-bold"
-        dateTime="2026-03-21"
-      >
-        03.21.2026
-      </time>
+      <hgroup className="relative z-10 text-center">
+        <h1 className="font-display text-[#FF39E1] mb-4 md:mb-8 min-[1920px]:mb-10.5 text-display font-semibold">
+          Touchpoint 2026
+        </h1>
+        <p className="text-description font-Haas text-touchpoint-green mb-2 md:mb-4 min-[1920px]:mb-6 ">
+          SFU Surrey Engineering Building
+        </p>
+        <time
+          className="text-touchpoint-green text-description font-bold"
+          dateTime="2026-03-21"
+        >
+          03.21.2026
+        </time>
+      </hgroup>
 
-      <div className="absolute font-Haas bottom-16 gap-5 flex flex-col md:flex-row items-center">
-        <span className="text-[1rem] text-muted-foreground">In partnership with</span>
+      <div className="absolute z-10 font-Haas bottom-16 gap-5 flex flex-col md:flex-row items-center">
+        <span className="text-[1rem] text-muted-foreground">
+          In partnership with
+        </span>
 
-        <Image className="max-w-30 md:max-w-40" src={circleInnovation} alt="Circle Innovation" />
+        <Image
+          className="max-w-30 md:max-w-40"
+          src={circleInnovation}
+          alt="Circle Innovation"
+        />
       </div>
 
+      <div className="absolute inset-0">
+        <OGLCanvas>
+          <Shader />
+        </OGLCanvas>
+      </div>
     </motion.div>
   );
 }
+
+const Shader = () => {
+  const { gl, canvas, renderer } = useOGL();
+
+  const renderTarget = useMemo(() => new RenderTarget(gl), []);
+
+  const mousePos = useRef({ old: { x: 0, y: 0 }, new: { x: 0, y: 0 } });
+  const mouseAccel = useRef(0);
+
+  const mesh = new Mesh(gl, {
+    geometry: new Triangle(gl),
+    program: new Program(gl, {
+      vertex: basicVert,
+      fragment: basicFrag,
+      uniforms: {
+        uTime: { value: 0.0 },
+        uMouse: { value: [0.0, 0.0] },
+        uResolution: { value: [gl.canvas.width, gl.canvas.height] },
+        uMetablobs: { value: [] },
+        uSpeed: { value: matchMedia("(pointer:fine)").matches ? 0.5 : 4 },
+        uMobile: { value: matchMedia("(pointer:fine)").matches ? false : true },
+      },
+    }),
+  });
+
+  const metablobs = useRef(
+    Array.from(
+      { length: 50 },
+      () =>
+        new Vec3(
+          2 * Math.random() - 1.0,
+          2 * Math.random() - 1.0,
+          Math.random() * 0.3,
+          // 1.2
+        ),
+    ),
+  );
+
+  useEffect(() => {
+    if (matchMedia("(pointer:fine)").matches) {
+      const handleMouseMove = (e: MouseEvent) => {
+        const mO = mousePos.current.old;
+        const mN = mousePos.current.new;
+        console.log(
+          "mouse move",
+          Math.sqrt(Math.pow(mN.x - mO.x, 2) + Math.pow(mN.y - mO.y, 2)),
+        );
+        if (mouseAccel.current < 50)
+          mouseAccel.current +=
+            10 * Math.sqrt(Math.pow(mN.x - mO.x, 2) + Math.pow(mN.y - mO.y, 2));
+        const rect = gl.canvas.getBoundingClientRect();
+        const x = e.clientX / rect.width - 0.5;
+        // const y = 1.0 - e.clientY / gl.canvas.height;
+        const y = 0.5 - e.clientY / rect.height;
+        mousePos.current.old = { ...mousePos.current.new };
+        mousePos.current.new = { x, y };
+        metablobs.current.map((mb) => {
+          mb.x += Math.random() >= 0.5 ? 0.05 : -0.05;
+          mb.y += Math.random() >= 0.5 ? 0.05 : -0.05;
+        });
+      };
+      window.addEventListener("mousemove", handleMouseMove);
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+      };
+    }
+  }, []);
+
+  useFrame((root, time) => {
+    // console.log(root.renderer);
+    mesh.program.uniforms.uTime.value = time * 0.001;
+    mesh.program.uniforms.uMouse.value = [
+      mousePos.current.old.x,
+      mousePos.current.old.y,
+    ];
+    if (mouseAccel.current > 0.5) mouseAccel.current *= 0.9; // Apply damping to decelerate more naturally
+    if (matchMedia("(pointer:fine)").matches)
+      mesh.program.uniforms.uSpeed.value = mouseAccel.current;
+    // mesh.program.uniforms.uMouse.value = [
+    //   mousePositionN.current[0],
+    //   mousePositionN.current[1],
+    // ];
+    mesh.program.uniforms.uResolution.value = [
+      root.renderer.width,
+      root.renderer.height,
+    ];
+    mesh.program.uniforms.uMetablobs.value = metablobs.current;
+    renderer.render({ scene: mesh, target: renderTarget });
+  });
+
+  return (
+    <Fragment>
+      <FinalScene texture={renderTarget.texture} />
+    </Fragment>
+  );
+};
